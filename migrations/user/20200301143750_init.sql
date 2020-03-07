b132a0996db3e1f72b16493426b9b5209b439bc3
Nick Spain <nicholas.spain96@gmail.com>
2020-03-07 17:17:28 +1100

    Only indent SQL with 2 spaces

--- migrations/user/20200301143750_init.sql
+++ migrations/user/20200301143750_init.sql
@@ -1,12 +1,12 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
 CREATE TABLE users (
-         id SERIAL              PRIMARY KEY
-       , email     VARCHAR(256) NOT NULL UNIQUE
-       , firstName VARCHAR(256) NOT NULL
-       , lastName  VARCHAR(256) NOT NULL
-       , password  VARCHAR(100) NOT NULL
-       , salt      VARCHAR(15)  NOT NULL UNIQUE
+  id          SERIAL       PRIMARY KEY
+  , email     VARCHAR(256) NOT NULL UNIQUE
+  , firstName VARCHAR(256) NOT NULL
+  , lastName  VARCHAR(256) NOT NULL
+  , password  VARCHAR(100) NOT NULL
+  , salt      VARCHAR(15)  NOT NULL UNIQUE
 );
 
 -- +goose Down
